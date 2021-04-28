0d4b31e3a5bff8ce1f39402fbd397fc8f17f8e26
Вячеслав Островский <slslava007@gmail.com>
2021-04-28 17:08:49 +0300

    Add project files.

---
+++ NumericalSequence/NumericalSequence.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalSequence
+{
+    class NumericalSequence
+    {
+        private uint n;
+        private uint m;
+
+        public NumericalSequence() { }
+        public NumericalSequence(uint n, uint m)
+        {
+            this.n = n;
+            this.m = m;
+        }
+        public void printRange()
+        {
+            Console.WriteLine("Ряд чисел: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                if (Math.Pow(i, 2) > m)
+                    Console.Write(i + ",");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ NumericalSequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n;
+            uint m;
+
+            Console.Write("Введите длину ряда: ");
+            n = Convert.ToUInt32(Console.ReadLine());
+            Console.Write("Введите значение минимального квадрата: ");
+            m = Convert.ToUInt32(Console.ReadLine());
+
+            NumericalSequence numericalSequence = new NumericalSequence(n, m);
+            numericalSequence.printRange();
+
+            Console.WriteLine("Программа завершена");
+            Console.ReadKey();
+        }
+    }
+}
